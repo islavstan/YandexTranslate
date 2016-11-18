e798a9dfd33866c4653b4e3da9f43efe60f608b9
islavstan <islavstan@gmail.com>
2016-11-18 21:37:08 +0200

    18.11

--- app/src/main/java/com/islavdroid/yandextranslate/MainActivity.java
+++ app/src/main/java/com/islavdroid/yandextranslate/MainActivity.java
@@ -6,6 +6,7 @@ import android.support.v7.app.NotificationCompat;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageButton;
 import android.widget.TextView;
 
 import java.util.ArrayList;
@@ -17,14 +18,38 @@ import retrofit2.Response;
 public class MainActivity extends AppCompatActivity {
     private EditText editText;
     private Button button;
-    private TextView textView;
-    private final String KEY="Key";
-    private final String LANG ="en-ru";
+    private ImageButton changeLanguageButton;
+
+    private TextView textView,rusText,englishText;
+    private final String KEY="trnsl.1.1.20161118T100805Z.31fde225bb41c01e.68f76975a93052046af80392f74d89f9bb8d439c";
+    private String LANG ="en-ru";
+    private boolean engLang =true;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        rusText=(TextView)findViewById(R.id.rusText) ;
+        englishText=(TextView)findViewById(R.id.englishText) ;
+        changeLanguageButton=(ImageButton)findViewById(R.id.changeLanguageButton);
+
+        changeLanguageButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(engLang){
+                    rusText.setText("англ");
+                    englishText.setText("рус ");
+                    LANG="ru-en";
+                    engLang=false;
+                }else {
+                    englishText.setText("англ");
+                    rusText.setText("рус ");
+                    LANG="en-ru";
+                    engLang=true;
+                }
+            }
+        });
+
         editText=(EditText)findViewById(R.id.edit_text);
         button =(Button)findViewById(R.id.translate_button);
         textView =(TextView)findViewById(R.id.translate_text);
